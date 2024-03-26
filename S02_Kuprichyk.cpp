51c0c900d2c83d19b7c4578fa6eee4191aa128d8
Ladislows <163967814+Ladislows@users.noreply.github.com>
2024-03-26 20:03:03 +0800

    Add files via upload

---
+++ S02_Kuprichyk.cpp
@@ -0,0 +1,12 @@
+//Show the operation of the division operators completely and
+//the determination of the remainder of the division.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+      int A = 4 , B = 13;
+         int C = B / A;  // the division operators
+         int D = B % A;  // the remainder of the division
+  cout << C << " " << D << endl;
+}
