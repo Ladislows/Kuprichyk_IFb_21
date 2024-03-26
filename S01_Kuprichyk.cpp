46ec9aea60b4c26764a6eac1b3b271c3b74fc69d
Ladislows <163967814+Ladislows@users.noreply.github.com>
2024-03-26 19:37:16 +0800

    Add files via upload

---
+++ S01_Kuprichyk.cpp
@@ -0,0 +1,25 @@
+// Create 4 variables of different types,
+// demonstrate the operation of four arithmetic operators to choose from.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Create 4 variables of different types
+    int A = 4 ; 
+    cout << " int variable value = " << A ;
+    float B = 3.2 ;
+    cout << " float variable value = " << B << '\n' ;
+    double C = 5.765 ;
+    cout << " double variable value = " << C  ;
+    char D = '7';
+    cout << " char variable value = " << D << '\n' ;
+
+// Demonstrate the operation of four arithmetic operators  
+ 
+     int a1 = 4 , a2 = 60 , a3 = 28 ;
+     cout << " The addition operator = a1+a3 =" << a1 + a3 << '\n';
+     cout << " The subtraction operator = a2-a3 =" << a2 - a3 << '\n' ;
+     cout << " The multiplication operator = a1*a3 =" << a1 * a3 << '\n' ;
+     cout << " The division operator = a2/a1 =" << a2 / a1 << '\n' ;
+}
