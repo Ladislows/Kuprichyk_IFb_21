805d86da8b080dfbfb114dcfc8b63a97ea33e89b
Ladislows <163967814+Ladislows@users.noreply.github.com>
2024-04-26 19:01:03 +0800

    Add files via upload

---
+++ s12_Kuprichyk.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int Sum=0 , Sum1=0;
+    for (int i = 0; i <1000 ; i+=4){
+            if (i%7==0){
+                Sum+=i;    // counting from 0
+  }
+}
+   for (int i = 1; i <1000 ; i+=4){
+            if (i%7==0){
+                Sum1+=i;  // counting from 1
+  }
+}
+    cout<<Sum<< endl;
+    cout<<Sum1;
+    return 0;
+}
