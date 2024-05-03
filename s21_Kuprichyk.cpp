435e679b0ba32236a3091aaf19dcd44cea56ccaa
Ladislows <163967814+Ladislows@users.noreply.github.com>
2024-05-03 22:11:02 +0800

    Update s21_Kuprichyk.cpp

--- s21_Kuprichyk.cpp
+++ s21_Kuprichyk.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
-int main ()
+
+int main()
 {
-   int A,B, sum = 0;
-   cin >> A >> B;
-   for (int i = A; i <=B;  i++)
-   {
-      cout << i*3 << " ";
-   }
+    ofstream file;
+
+    int A,B, i;
+    cout <<"Введите А ";
+    cin >> A;
+    cout <<"Введите B ";
+    cin >> B;
+     for (int i = A; i <=B;  i++){
+    cout << i*3 << " ";
+    }
+    file.open("output.txt");
+    file  << i*3 << " ";
+    file.close();
+    cin.get();
+    return 0;
 }
