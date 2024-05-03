4858f5e890abe761dcd5875fddbaf3d9ad8b4b6b
Ladislows <163967814+Ladislows@users.noreply.github.com>
2024-05-03 22:55:59 +0800

    Update s51_Kuprichyk.cpp

--- s51_Kuprichyk.cpp
+++ s51_Kuprichyk.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
 #include<string>
-using namespace std; 
-int main() 
-{ 
-    string reverse="Testing"; 
+#include <clocale>
+using namespace std;
+
+string reverse(string str);
+
+int main(){
+   setlocale(LC_ALL, "RUS");
+   string str;
+   cout << "Введите строчку :" << endl;
+   cin >> str;
+   cout << "Строчка в обратном порядке:" << endl;
+   cout << reverse(str);
+   return 0;
+}
+
+string reverse(string str) {
     int i;
-    for(i = reverse.length() - 1; i >= 0; i--)
-    {
-       cout<<reverse[i];
+    for (i = str.length() - 1; i >= 0; i--) {
+        cout <<  str[i];
     }
-    return 0;
 }
