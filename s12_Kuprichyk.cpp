a61e1a0371441ab5869fec111869c86046695766
Ladislows <163967814+Ladislows@users.noreply.github.com>
2024-05-03 21:56:00 +0800

    Update s12_Kuprichyk.cpp

--- s12_Kuprichyk.cpp
+++ s12_Kuprichyk.cpp
@@ -7,13 +7,13 @@ int main(){
     for (int i = 0; i <1000 ; i+=4){
             if (i%7==0){
                 Sum+=i;    // counting from 0
-  }
-}
+            }
+    }
    for (int i = 1; i <1000 ; i+=4){
             if (i%7==0){
                 Sum1+=i;  // counting from 1
-  }
-}
+            }
+    }
     cout<<Sum<< endl;
     cout<<Sum1;
     return 0;
