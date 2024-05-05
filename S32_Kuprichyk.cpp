dc6e4d53469f4506dccf62f1678aefab3cffe035
Ladislows <163967814+Ladislows@users.noreply.github.com>
2024-05-05 19:41:07 +0800

    Add files via upload

---
+++ S32_Kuprichyk.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    char str[100];
+    cout << "Введите строку: ";
+    cin >> str;
+
+    int len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        str[i]++;
+    }
+    cout << "Получившееся строка: " << str << std::endl;
+     return 0;
+}
